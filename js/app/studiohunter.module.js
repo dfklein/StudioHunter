1bf9433a7acb5e60510b0889ed6a9e4293094b3f
dfklein <dklein@gmail.com>
2017-08-26 20:25:24 -0300

    Rotas

--- js/app/studiohunter.module.js
+++ js/app/studiohunter.module.js
@@ -2,13 +2,13 @@ var studioHunterApp = angular.module('StudioHunterApp', ['ngRoute']);
 
 studioHunterApp.config(function($routeProvider, $locationProvider) {
 	$routeProvider
-	.when('/home', {
+	.when('/', {
 		// os parâmetros abaixo são respectivamente a URL chamada e o controller utilizado. Desta forma você não utilizaria o "ng-controller" no html. Ver a página cliente.html
 		templateUrl:'../view/home.html',
 		controller:'clienteController'
 	})
 //	.otherwise({
-//		redirectTo:'/'
+//		redirectTo:'/home'
 //	});
 //	
 //	// as duas linhas abaixo eliminam o #!(versão 1.6.X do Angular) ou #(outras versões) da URL nas chamadas sem requisição para o servidor.
