3068168549322d30fd1610acc0ccfb9549802c7f
dfklein <dklein@gmail.com>
2017-08-26 23:21:36 -0300

    Desenvolvimento da home

--- js/app/studiohunter.module.js
+++ js/app/studiohunter.module.js
@@ -1,27 +1,23 @@
 var studioHunterApp = angular.module('StudioHunterApp', ['ngRoute']); 
 
 studioHunterApp.config(function($routeProvider, $locationProvider) {
+//	var PATH = '../view/';
+	var PATH = '../dfklein.github.io/view/';
+	
+	
 	$routeProvider
 	.when('/', {
 		// os parâmetros abaixo são respectivamente a URL chamada e o controller utilizado. Desta forma você não utilizaria o "ng-controller" no html. Ver a página cliente.html
-		templateUrl:'../view/home.html',
+		templateUrl: PATH + 'home.html',
 		controller:'clienteController'
 	})
-//	.otherwise({
-//		redirectTo:'/home'
-//	});
+	.otherwise({
+		redirectTo:'/',
+		controller:'clienteController'
+	});
 //	
 //	// as duas linhas abaixo eliminam o #!(versão 1.6.X do Angular) ou #(outras versões) da URL nas chamadas sem requisição para o servidor.
 //	$locationProvider.hashPrefix('');
 //	$locationProvider.html5Mode(true);
 
-});
-
-// mock de navegação para rodar em arquivos locais.
-studioHunterApp.run(function($templateCache) {
-//	var x = $templateCache.get('../view/home.html');
-//	var y = $templateCache.get('c:\');
-//	console.log(x);
-//	console.log(y);
-//  $templateCache.put('/view/home.html', 'In the MainView partial <strong>{{message}}</strong>');
 });
